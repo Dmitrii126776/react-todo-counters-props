fadf81b4db6f43be08a9ab780c07b6f1adc1c745
Dmitrii126776 <dukek67@gmail.com>
2022-12-03 11:15:59 -0500

    one-page app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+import {v4 as uuidv4} from 'uuid';
 import './App.css';
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [counters, setCounters] = useState([
+        {id: uuidv4(), name: 'orange', value: 22}])
+    const [inputName, setInputName] = useState('')
+    const [inputNumber, setInputNumber] = useState('')
+    const addCounter = () => {
+        const newCounter = {id: uuidv4(), name: inputName, value: Number(inputNumber) || 0}
+        setCounters([...counters, newCounter])
+        setInputName('')
+        setInputNumber('')
+    }
+    const minus = (id) => {
+        setCounters(counters.map(el => el.id === id ? {...el, value: el.value - 1} : el))
+    }
+    const plus = (id) => {
+        setCounters(counters.map(el => el.id === id ? {...el, value: el.value + 1} : el))
+    }
+    const deleteCounter = (id) => {
+        setCounters(counters.filter(el => el.id !== id))
+    }
+
+    return (
+        <div className="App">
+            <h2>Fruits Counters</h2>
+            <div>
+                <input type='text' placeholder='fruit name'
+                       value={inputName}
+                       onChange={(e) => setInputName(e.target.value)}/>
+                <input type='text' placeholder='fruit number'
+                       value={inputNumber}
+                       onChange={(e) => setInputNumber(e.target.value)}/>
+                <button onClick={addCounter}>Add Fruits</button>
+            </div>
+            <ol>
+                {counters.map(el =>
+                    (
+                        <li key={el.id}>
+                            {el.name}
+                            <button onClick={() => minus(el.id)}>-</button>
+                            {el.value}
+                            <button onClick={() => plus(el.id)}>+</button>
+                            <button onClick={() => deleteCounter(el.id)}>Delete</button>
+                        </li>
+                    ))}
+            </ol>
+        </div>
+    );
 }
 
 export default App;
